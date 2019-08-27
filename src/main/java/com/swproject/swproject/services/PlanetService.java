007226edc9f5e7e9ef5160b8ac28639730bd5914
raysilent7 <rgc471@hotmail.com>
2019-08-27 16:54:22 -0300

    findById

--- src/main/java/com/swproject/swproject/services/PlanetService.java
+++ src/main/java/com/swproject/swproject/services/PlanetService.java
@@ -2,10 +2,12 @@ package com.swproject.swproject.services;
 
 import com.swproject.swproject.domain.Planet;
 import com.swproject.swproject.repository.PlanetRepository;
+import com.swproject.swproject.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlanetService {
@@ -15,5 +17,10 @@ public class PlanetService {
 
     public List<Planet> findAll() {
         return repo.findAll();
+    }
+
+    public Planet findById(String id) {
+        Optional<Planet> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
---
+++ src/main/java/com/swproject/swproject/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.swproject.swproject.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}

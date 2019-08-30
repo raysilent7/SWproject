e7516f64079a68833bfdfea9c0790578c3e9f7d9
raysilent7 <rgc471@hotmail.com>
2019-08-30 12:12:27 -0300

    customized validation error

--- src/main/java/com/swproject/swproject/resources/exception/ResouceExceptionHandler.java
+++ src/main/java/com/swproject/swproject/resources/exception/ResouceExceptionHandler.java
@@ -4,6 +4,8 @@ import com.swproject.swproject.services.exception.BadRequestException;
 import com.swproject.swproject.services.exception.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -28,5 +30,18 @@ public class ResouceExceptionHandler {
         StandardErrorMsg err = new StandardErrorMsg(System.currentTimeMillis(), status.value(),
                  e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> argumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ValidationError err = new ValidationError(System.currentTimeMillis(), status.value(), "Validation error", request.getRequestURI());
+
+        for (FieldError x : e.getBindingResult().getFieldErrors()) {
+            err.addError(x.getField(), x.getDefaultMessage());
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
---
+++ src/main/java/com/swproject/swproject/resources/exception/ValidationError.java
@@ -0,0 +1,21 @@
+package com.swproject.swproject.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationError extends StandardErrorMsg {
+
+    private List<FieldMessage> errors = new ArrayList<>();
+
+    public ValidationError(Long timestamp, Integer status, String message, String path) {
+        super(timestamp, status, message, path);
+    }
+
+    public List<FieldMessage> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String message) {
+        errors.add(new FieldMessage(fieldName, message));
+    }
+}
